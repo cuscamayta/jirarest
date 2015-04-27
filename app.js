6669abbaa1ffc8764884dbbf80b4c0eb96725f4c
Carlos Uscamayta <cuscamayta@innsa.com.bo>
2015-04-27 08:55:55 -0400

    Init Jira rest Api Heroku

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express'),
+	app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, resp) {
+	resp.sendfile('index.html');
+});
+
+app.listen(7000, function() {
+
+	console.log('servidor iniciario en el puerto 7000');
+});
